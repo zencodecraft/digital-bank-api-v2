3d37ca5f05ecbe7b57b35c0f526c78481d585ffa
Bruna <brunamoreira@artedeviver.org.br>
2025-05-08 15:28:47 -0300

    Primeiro commit - build on

--- bancodigital/src/main/java/br/com/cdb/bancodigital/BancodigitalApplication.java
+++ bancodigital/src/main/java/br/com/cdb/bancodigital/BancodigitalApplication.java
@@ -1,4 +1,4 @@
-package br.com.cdb.bancodigital;
+git remote -vpackage br.com.cdb.bancodigital;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
